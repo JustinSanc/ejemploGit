7c4e586c1a437064a1c407344d9cdedbd908bfa9
JustinSanc <justin_sr@tesch.edu.mx>
2024-12-05 14:59:19 -0600

    Update MainActivity.kt

--- app/src/main/java/com/example/ejemplogit/MainActivity.kt
+++ app/src/main/java/com/example/ejemplogit/MainActivity.kt
@@ -22,6 +22,7 @@ class MainActivity : AppCompatActivity() {
         Toast.makeText(this, "¡Bienvenido a EjemploGit! 🌟", Toast.LENGTH_LONG).show()
         Toast.makeText(this, "Exelente Ejemplo", Toast.LENGTH_LONG).show()
         Toast.makeText(this,"Tester",Toast.LENGTH_SHORT);
+        Toast.makeText(this,"Hola",Toast.LENGTH_SHORT);
         //mio
     }
 }
